10267160ad5aaf767feacf638b0dbc6fe95bf2f8
jeydo6 <Deryagin.Valdemar@yandex.ru>
2021-01-22 11:44:25 +0300

    Lesson # 59.8

--- WayOfSamurai.WebAPI/jest.config.js
+++
@@ -1,5 +0,0 @@
-export const preset = 'ts-jest';
-export const testEnvironment = 'node';
-export const roots = ['<rootDir>/tests'];
-export const setupFiles = ['<rootDir>/tests/setup.ts'];
-export const collectCoverageFrom = ['<rootDir>/src/**/*.ts', '!**/node_modules/**'];
--- WayOfSamurai.WebAPI/src/server.ts
+++
@@ -1,7 +0,0 @@
-import Logger from './core/Logger';
-import { port } from './config';
-import app from './app';
-
-app.listen(port, () => {
-    Logger.info(`server running in port: ${port}`);
-}).on('error', (e) => Logger.error(e));
